1b4cb579a297dabf5133eed8406d64fc7614f5a7
kcarvin43 <57299777+kcarvin43@users.noreply.github.com>
2019-11-23 15:00:08 -0500

    added quick math .py file - also set a function to call

---
+++ qm.py
@@ -0,0 +1,16 @@
+# quick set of random variables
+a = 5
+b = 4
+c = a + b
+d = a - b
+e = a + b + c + d
+
+# defining solution function to show us the output above
+def solution():
+    return e
+
+# setting the defined function as variable x for easier calling
+x = solution()
+
+# lets see what we get
+print(x)
